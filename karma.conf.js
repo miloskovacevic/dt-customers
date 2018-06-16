7f808773c139606a3bb4293941d950928c8bfec1
Miloš Kovačević <miloscpt@gmail.com>
2018-06-17 01:12:48 +0200

    Changed Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,18 @@
 
 module.exports = function (config) {
   config.set({
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+     // you can define custom flags
+     customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+   
+    singleRun: true,
+
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
