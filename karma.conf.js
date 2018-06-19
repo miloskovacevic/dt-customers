230af5c3a12d3478e5627e801c15515bd21672da
Miloš Kovačević <miloscpt@gmail.com>
2018-06-19 17:34:51 +0200

    changed karma file again and yml

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox', 'ChromeNoSandbox'],
      customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
